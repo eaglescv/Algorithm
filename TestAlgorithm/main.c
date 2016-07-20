4fc6a7c3ce720301a58c0d47330e782408adc6b4
eaglescv <eaglescv@naver.com>
2016-07-20 18:36:29 +0900

    leap years add

--- TestAlgorithm/main.c
+++ TestAlgorithm/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+enum EWEEK
+{
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+};
+
 int item[99] = { 0, };
 
 int GaussAlgorithm()
@@ -34,19 +45,43 @@ int IsPalindrome(char* inputString)
 	return 1;
 }
 
+int IsLeapYears(int years)
+{
+	if ((years % 4 == 0 && years % 100 != 0) || years % 400 == 0)
+	{
+		return 1;
+	}
+	return 0;
+}
+
+int GetDayOfTheWeek(int years, int month, int day)
+{
+	int result = 0;
+
+	switch (result)
+	{
+	default:
+		break;
+	}
+	return result;
+}
+
 void main(int argc, char** args)
 {
 	//int gaussTotal = GaussAlgorithm();
 	//printf("%d\n", gaussTotal);
 
-	int result = IsPalindrome(args[1]);
-	if (result)
-	{
-		printf("it is palindrome\n");
-	}
-	else
-	{
-		printf("it is NOT palindrome\n");
-	}
+	//int result = IsPalindrome(args[1]);
+	//if (result)
+	//{
+	//	printf("it is palindrome\n");
+	//}
+	//else
+	//{
+	//	printf("it is NOT palindrome\n");
+	//}
+
+	//DoomsDay doomsday;
+
 	getchar();
 }
