971d8ca569eb06baec0a0cb3eb5da895bf498fa9
eaglescv <eaglescv@naver.com>
2016-08-05 15:52:53 +0900

    쇠막대기 시간초과 오류 수정

--- 쇠막대기/main.cpp
+++ 쇠막대기/main.cpp
@@ -1,115 +1,157 @@
-#include <stdio.h>
-#include <string>
-#include <iostream>
-#include <vector>
+//#include <stdio.h>
+//#include <string>
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//struct Line
+//{
+//	int start;
+//	int end;
+//};
+//
+//vector<Line> LineVec;
+//
+//bool IsLajor(string& buffer, int currentIndex)
+//{
+//	if ((int)buffer.size() <= 0)
+//	{
+//		return false;
+//	}
+//
+//	if (buffer[currentIndex] == ')')
+//	{
+//		if (buffer[currentIndex - 1] == '(')
+//		{
+//			return true;
+//		}
+//	}
+//	return false;
+//}
+//
+//int GetLajorCount(string& buffer, int startIndex, int endIndex)
+//{
+//	int count = 0;
+//	for (int i = startIndex + 1; i < endIndex; i++)
+//	{
+//		if (IsLajor(buffer, i))
+//		{
+//			count++;
+//		}
+//	}
+//	return count;
+//}
+//
+//void MakeLine(string& buffer, int startIndex)
+//{
+//	int count = 0;
+//	int i;
+//	for (i = startIndex; i < (int)buffer.size(); i++)
+//	{
+//		if (buffer[i] == '(')
+//		{
+//			count++;
+//		}
+//		else if (buffer[i] == ')')
+//		{
+//			count--;
+//		}
+//
+//		if (count == 0)
+//		{
+//			break;
+//		}
+//	}
+//
+//	if (i - startIndex > 1)
+//	{
+//		Line line;
+//		line.start = startIndex;
+//		line.end = i;
+//		LineVec.push_back(line);
+//	}
+//}
+//
+//void TotalMakeLine(string& buffer)
+//{
+//	for (int i = 0; i < (int)buffer.size(); i++)
+//	{
+//		if (buffer[i] == '(')
+//		{
+//			MakeLine(buffer, i);
+//		}
+//	}
+//}
+//
+//void Print(string& buffer)
+//{
+//	//막대기 수를 구한다.
+//	//for( int i = 0; i < 막대기 수; i++)
+//	//{
+//	//  total += (막대기 안에 레이져 갯수를 구하고 1을 더한다.)
+//	//}
+//
+//	int totalLineNum = 0;
+//	TotalMakeLine(buffer);
+//	for (int i = 0; i < (int)LineVec.size(); i++)
+//	{
+//		Line& line = LineVec[i];
+//		totalLineNum += GetLajorCount(buffer, line.start, line.end);
+//		totalLineNum += 1;
+//	}
+//	cout << totalLineNum << endl;
+//}
+//
+//int main()
+//{
+//	string str = "()(((()())(())()))(())";
+//	//string str = "(((()(()()))(())()))(()())";
+//	//string str;
+//	//cin >> str;
+//
+//	Print(str);
+//	//return 0;
+//	getchar();
+//}
 
+#include <iostream> 
+#include <string> 
+#include <stack> 
 using namespace std;
-struct Line
+
+int main() 
 {
-	int start;
-	int end;
-};
+	string a = "()(((()())(())()))(())";
+	//cin >> a;
 
-vector<Line> LineVec;
+	int n = a.size();
+	stack<int> s;
+	int ans = 0;
+	int top = 0;
 
-bool IsLajor(string& buffer, int currentIndex)
-{
-	if ((int)buffer.size() <= 0)
+	for (int i = 0; i < n; i++) 
 	{
-		return false;
-	}
-
-	if (buffer[currentIndex] == ')')
-	{
-		if (buffer[currentIndex - 1] == '(')
+		if (a[i] == '(') 
 		{
-			return true;
+			s.push(i);
 		}
-	}
-	return false;
-}
-
-int GetLajorCount(string& buffer, int startIndex, int endIndex)
-{
-	int count = 0;
-	for (int i = startIndex + 1; i < endIndex; i++)
-	{
-		if (IsLajor(buffer, i))
+		else 
 		{
-			count++;
-		}
-	}
-	return count;
-}
-
-void MakeLine(string& buffer, int startIndex)
-{
-	int count = 0;
-	int i;
-	for (i = startIndex; i < (int)buffer.size(); i++)
-	{
-		if (buffer[i] == '(')
-		{
-			count++;
-		}
-		else if (buffer[i] == ')')
-		{
-			count--;
-		}
-
-		if (count == 0)
-		{
-			break;
+			top = s.top();
+			if (top + 1 == i)
+			{
+				s.pop();
+				ans += s.size();
+			}
+			else 
+			{
+				s.pop();
+				ans += 1;
+			}
 		}
 	}
 
-	if (i - startIndex > 1)
-	{
-		Line line;
-		line.start = startIndex;
-		line.end = i;
-		LineVec.push_back(line);
-	}
-}
-
-void TotalMakeLine(string& buffer)
-{
-	for (int i = 0; i < (int)buffer.size(); i++)
-	{
-		if (buffer[i] == '(')
-		{
-			MakeLine(buffer, i);
-		}
-	}
-}
-
-void Print(string& buffer)
-{
-	//막대기 수를 구한다.
-	//for( int i = 0; i < 막대기 수; i++)
-	//{
-	//  total += (막대기 안에 레이져 갯수를 구하고 1을 더한다.)
-	//}
-
-	int totalLineNum = 0;
-	TotalMakeLine(buffer);
-	for (int i = 0; i < (int)LineVec.size(); i++)
-	{
-		Line& line = LineVec[i];
-		totalLineNum += GetLajorCount(buffer, line.start, line.end);
-		totalLineNum += 1;
-	}
-	cout << "Total Line Num:" << totalLineNum << endl;
-}
-
-int main()
-{
-	string str = "()(((()())(())()))(())";
-	//string str = "(((()(()()))(())()))(()())";
-	//string str;
-	//cin >> str;
-
-	Print(str);
+	cout << ans << endl;
 	//return 0;
 	getchar();
 }
