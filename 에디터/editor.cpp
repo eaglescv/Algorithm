2e4b1adb613c37754d572b654c8f9bb303cc8b43
eaglescv <eaglescv@naver.com>
2016-08-06 02:19:11 +0900

    에디터 수정 - 출력까지 완료

--- 에디터/editor.cpp
+++ 에디터/editor.cpp
@@ -1,71 +1,77 @@
-#include <stdio.h>
-#include <iostream>
-#include <string>
+#include <cstdio>
+#include <cstring>
 #include <stack>
-#include <queue>
 
 #pragma warning(disable:4996)
 using namespace std;
-queue<string> CommandQueue;
+
 stack<char> LeftStack;
 stack<char> RightStack;
 
 int main()
 {
-	string str = "abcd";
-	//cin >> str;
-
-	for (int i = 0; i < (int)str.size(); i++)
+	char c[600000];
+	scanf("%s", &c);
+	
+	int num = strlen(c);
+	for (int i = 0; i < num; i++)
 	{
-		LeftStack.push(str[i]);
-	}
-
-	int commandCount = 0;
-	scanf("%d\n", &commandCount);
-
-	char command[10];
-
-	for (int i = 0; i < commandCount; i++)
-	{
-		gets(command);
-		if (strlen(command) <= 0)
-			continue;
-
-		CommandQueue.push(command);
-	}
-
-	while (false == CommandQueue.empty())
-	{
-		string& command = CommandQueue.front();
-		char val;
-		switch (command[0])
-		{
-		case 'L':
-			val = LeftStack.top();
-			RightStack.push(val);
-			LeftStack.pop();
-			break;
-		case 'D':
-			val = RightStack.top();
-			LeftStack.push(val);
-			RightStack.pop();
-			break;
-		case 'B':
-			LeftStack.pop();
-			break;
-		case 'P':
-			if (command[2])
-			{
-				LeftStack.push(command[2]);
-			}
-			break;
-		default:
-			break;
-		}
-		CommandQueue.pop();
+		LeftStack.push(c[i]);
 	}
 	
-	int i = 0;
-	//return 0;
+	int commandCount;
+	scanf("%d", &commandCount);
+
+	while (commandCount--)
+	{
+		char command;
+		scanf(" %c", &command);
+
+		switch (command)
+		{
+		case 'L':
+			if (false == LeftStack.empty())
+			{
+				RightStack.push(LeftStack.top());
+				LeftStack.pop();
+			}
+			break;
+		case 'D':
+			if (false == RightStack.empty())
+			{
+				LeftStack.push(RightStack.top());
+				RightStack.pop();
+			}
+			break;
+		case 'B':
+			if (false == LeftStack.empty())
+			{
+				LeftStack.pop();
+			}
+			break;
+		case 'P':
+			char str;
+			scanf(" %c", &str);
+			LeftStack.push(str);
+			break;
+		}
+	}
+	
+	while (false == LeftStack.empty())
+	{
+		RightStack.push(LeftStack.top());
+		LeftStack.pop();
+	}
+
+	while (false == RightStack.empty())
+	{
+		printf("%c", RightStack.top());
+		RightStack.pop();
+	}
+
+	printf("\n");
+	
 	getchar();
+	//return 0;
+
 }
