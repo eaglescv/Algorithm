e007ffed30055927095d7ceba378719b28496e4a
eaglescv <eaglescv@naver.com>
2016-09-23 17:44:19 +0900

    코드 수정

--- LIS_최장증가수열/main.cpp
+++ LIS_최장증가수열/main.cpp
@@ -15,10 +15,57 @@
 //예시 입력파일은
 
 //7
-
 //1 3 2 4 9 7 3
 
 //이었고, 결과물 출력파일의 답은
 //4
 //이다.
 //---------------------------------------------------------------------------------
+#include <iostream>
+
+using namespace std;
+//---------------------------------------------------------------------------------
+//LIS 최장 증가 수열
+//BOJ - https://www.acmicpc.net/problem/11053
+//---------------------------------------------------------------------------------
+int main()
+{
+	int n, MAX = 0, i, j;
+	int data[10000] = { 0, };
+	int LS[10000] = { 0, };
+
+	//ifstream in;
+	//ofstream out;
+
+	//in.open("input.txt");
+	//out.open("output.txt");
+
+	cin >> n;
+	for (i = 1; i <= n; ++i)
+		cin >> data[i];
+
+	for (i = 1; i <= n; ++i)
+	{
+		LS[i] = 1;
+		for (j = 1; j <= i; ++j)
+		{
+			if ((data[j] < data[i]) && (LS[j] + 1 > LS[i]))
+			{
+				LS[i] = LS[j] + 1;
+			}
+		}
+	}
+
+	for (i = 1; i <= n; ++i)
+	{
+		if (MAX < LS[i])
+			MAX = LS[i];
+	}
+
+	cout << MAX;
+
+	//in.close();
+	//out.close();
+
+	return 0;
+}
