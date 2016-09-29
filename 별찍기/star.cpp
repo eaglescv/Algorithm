5480beee3dd4b903a52ef98582bebba8b612219c
eaglescv <eaglescv@naver.com>
2016-09-29 18:53:41 +0900

    별찍기 5번추가

--- 별찍기/star.cpp
+++ 별찍기/star.cpp
@@ -50,11 +50,27 @@ void PrintStar2(int n)
 			printf("%c", i + j <= n ? ' ' : '*');
 }
 
+//º°Âï±â - 5
+//https://www.acmicpc.net/problem/2442
+void PrintStar5(int n)
+{
+	for (int i = 1; i <= n; printf("\n", i++))
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			//2*n-1
+			//2*5-1 => 9
+
+		}
+	}
+}
+
 int main()
 {
 	int n = 0;
 	cin >> n;
 
-	PrintStar2(n);
+	//PrintStar2(n);
+	PrintStar5(n);
 	return 0;
 }
